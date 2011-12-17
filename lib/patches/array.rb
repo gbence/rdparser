a02c39183b36600c3a8ba0d066281c9ff60d94b4
Golda Bence <golda@bitandpixel.hu>
2011-12-17 20:52:54 +0100

    Fixes RDParser::ArrayExtensions.

--- lib/patches/array.rb
+++ lib/patches/array.rb
@@ -1,7 +1,9 @@
 # Provide Array with method that appends hashes, but concatenates arrays
-module RDParser::ArrayExtensions
-  def append_or_blend(i)
-    i.class == Hash ? self << i : self.concat(i)
+module RDParser
+  module ArrayExtensions
+    def append_or_blend(i)
+      i.class == Hash ? self << i : self.concat(i)
+    end
   end
 end
 
